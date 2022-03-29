eed730eeab9798f00d3c3928eea01c2903e17ff2
psssDev <46029476+kmigo@users.noreply.github.com>
2022-03-29 15:14:55 -0300

    train_x,train_y

--- aula4/main.py
+++ aula4/main.py
@@ -12,22 +12,18 @@ dog2 = [1,0,1]
 dog3 = [1,1,1]
 
 # 1=> pig, 0=> dog
-data = [pig1, pig2, pig3, dog1, dog2, dog3]
-
+train_x = [pig1, pig2, pig3, dog1, dog2, dog3]
+train_y =[1,1,1,0,0,0] # labels
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 
 model = LinearSVC()
 # data adaptation (data, classes)
-model.fit(data, [1,1,1,0,0,0])
+model.fit(train_x, train_y)
+test_x = [[1,1,1],[1,1,0],[1,0,1],[0,1,1]]
+test_y =[0,1,0,1]
 
 
-mysterious = [[1,1,1],[1,1,0],[1,0,1],[0,1,1]]
-classes =[0,1,0,1]
-predictions = model.predict(mysterious)
-
-# sum true´s
-correct = (predictions == classes).sum()
-total = len(classes)
-accuracy = correct/total
-print('Accuracy is: ', accuracy *100)
+predictions = model.predict(test_x)
+accuracy = accuracy_score(test_y, predictions )
+print('accuracy is: %.2f  '% (accuracy *100))
